e126c63a166464b35b133502e485a5428894e3c3
Adrian Moreno <amorenoz@redhat.com>
2023-07-04 15:53:05 +0200

    net: openvswitch: add meter drop reason
    By using an independent drop reason it makes it easy to ditinguish
    between QoS-triggered or flow-triggered drop.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- net/openvswitch/drop.h
+++ net/openvswitch/drop.h
@@ -11,6 +11,7 @@
 	R(OVS_DROP_FLOW)		        \
 	R(OVS_DROP_EXPLICIT_ACTION)		\
 	R(OVS_DROP_EXPLICIT_ACTION_ERROR)	\
+	R(OVS_DROP_METER)			\
 	/* deliberate comment for trailing \ */
 
 enum ovs_drop_reason {
